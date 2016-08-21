a834fea2b158b92fec40619ddc9f8c66cc0cc24f
grandyzzz <twieank@gmail.com>
2016-08-21 21:25:49 +0200

    Created GameView class

---
+++ O4mund-The-Game/app/src/main/java/com/kode/o4mundthegame/GameView.java
@@ -0,0 +1,14 @@
+package com.kode.o4mundthegame;
+
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView {
+    public GameView(Context context) {
+        super(context);
+    }
+}
+
+
+
--- O4mund-The-Game/app/src/main/java/com/kode/o4mundthegame/MainActivity.java
+++ O4mund-The-Game/app/src/main/java/com/kode/o4mundthegame/MainActivity.java
@@ -1,13 +1,21 @@
 package com.kode.o4mundthegame;
 
+        import android.content.pm.ActivityInfo;
+        import android.support.v4.content.pm.ActivityInfoCompat;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.Window;
+        import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
     int l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GameView(this));
     }
 }
